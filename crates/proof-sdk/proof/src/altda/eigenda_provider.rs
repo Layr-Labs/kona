c1662a59fc8506631290c6b6011fe64ad220cfaf
Ubuntu <ubuntu@ip-172-31-39-89.ec2.internal>
2024-12-10 18:51:22 +0000

    small change to ad elements

--- crates/proof-sdk/proof/src/altda/eigenda_provider.rs
+++ crates/proof-sdk/proof/src/altda/eigenda_provider.rs
@@ -39,4 +39,24 @@ impl<T: CommsClient + Sync + Send> EigenDABlobProvider for OracleEigenDAProvider
             .map_err(OracleProviderError::Preimage)?;        
         Ok(data.into())
     }
+
+    async fn get_element(&mut self, cert: &Bytes, element: &Bytes) -> Result<Bytes, Self::Error> {
+        self.oracle
+            .write(&HintType::AltDACommitment.encode_with(&[&cert]))
+            .await
+            .map_err(OracleProviderError::Preimage)?;
+
+        let cert_point_key = Bytes::copy_from_slice(&[cert.to_vec(), element.to_vec()].concat());
+        
+        self.oracle
+            .write(&HintType::AltDACommitment.encode_with(&[&cert_point_key]))
+            .await
+            .map_err(OracleProviderError::Preimage)?;
+        let data = self
+            .oracle
+            .get(PreimageKey::new(*keccak256(cert_point_key), PreimageKeyType::GlobalGeneric))
+            .await
+            .map_err(OracleProviderError::Preimage)?;        
+        Ok(data.into())
+    }
 }
